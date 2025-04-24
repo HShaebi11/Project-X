db35caff43bd22dfb20e9e77c1a6aa0ee1016b7b
Hamza <hshaebi.11@gmail.com>
2025-04-24 02:42:26 +0100

    Initial Commit

---
+++ ProjectX/ProjectXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectXApp.swift
+//  ProjectX
+//
+//  Created by Hamza Shaebi on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjectXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectXTests/ProjectXTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectXTests.swift
+//  ProjectXTests
+//
+//  Created by Hamza Shaebi on 24/04/2025.
+//
+
+import Testing
+
+struct ProjectXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
